e7cab272d6309108ca9bb6d836aa81e21ca893c3
Eden <110981975+edennnni@users.noreply.github.com>
2026-01-29 20:52:19 +0100

    lesson 21

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
-import BottomTabNavigator from "./src/navigation/TabNavigator";
-import DrawerNavigation from "./src/navigation/DrawerNavigation";
-
+import DrawerNavigator from "./src/navigation/DrawerNavigator";
+//import BottomTabNavigator from "./src/navigation/TabNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigation />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
 
+<
