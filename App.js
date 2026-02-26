268a3b78cca377e7214188283397434578b64525
Eden <110981975+edennnni@users.noreply.github.com>
2026-02-26 11:41:27 -0800

    lesson32233232
    lesswonn2332

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "react-native-web";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 import Bird from "./src/Components/Bird";
-import { useEffect } from "react";
+import Obstacles from "./src/Components/Obstacles";
+import { useEffect, useState } from "react";
 
   export default function App() {
   const screenWidth = Dimensions.get("window").width;
@@ -25,14 +26,33 @@ import { useEffect } from "react";
     const [obstacleLeftTwo, setObstacleLeftTwo] = useState(screenWidth)
     const [obstacleMegHeightTwo, setObstacleNegHeightTwo] = useState(-Math.random() * 100)
     let obstacleTimeIdTwo;
-    
+      
+    const [birdBottom, setBirdBottom] = useState(screenHeight / 2)
+      const [isGameOver, setIsGameOver] = useState(false)
+      const [score, SetScore] = useState(0)
+
+    const gameOver=() => {
+      setIsGameOver(true)
+      clearInterval(gameTimeId)
+      clearInterval(obstacleTimeId)
+      clearInterval(obstacleTimeIdTwo)
+    }
+
+
+    const Jump = () => {
+      if (!isGameOver && (birdBottom < screenHeight)) {
+        setBirdBottom(birdBottom => birdBottom + 50);
+      }
+    }
+
+
     useEffect(() => {
       if (obstacleLeft > -obstacleWidth) {
-        obstacleTimerId = setInterval(() => {
+        obstacleTimeId = setInterval(() => {
           setObstacleLeft(obstacleLeft => obstacleLeft - 5);
         }, 30);
         return () => {
-          clearInterval(obstacleTimerId);
+          clearInterval(obstacleTimeId);
         };
       } else {
         setObstacleLeft(screenWidth);
@@ -40,19 +60,47 @@ import { useEffect } from "react";
       }
     }, [obstacleLeft]);
 
+     
     useEffect(() => {
       if (obstacleLeftTwo > -obstacleWidth) {
-        obstacleTimerIdTwo = setInterval(() => {
+        obstacleTimeIdTwo = setInterval(() => {
           setObstacleLeftTwo(obstacleLeftTwo => obstacleLeftTwo - 5);
         }, 30);
         return () => {
-          clearInterval(obstacleTimerIdTwo);
+          clearInterval(obstacleTimeIdTwo);
         };
       } else {
+        SetScore(score => score + 1)
         setObstacleLeftTwo(screenWidth);
         setObstacleNegHeightTwo(-Math.random() * 100);
       }
     }, [obstacleLeftTwo]);
+
+    useEffect(() => {
+      if (
+        (birdBottom < obstacleHeight + obstacleMegHeight || birdBottom > obstacleHeight + obstacleMegHeight + gap) &&
+        obstacleLeft < birdLeft + 30 &&
+        obstacleLeft + obstacleWidth > birdLeft
+      ) {
+        gameOver();
+      }
+    }, [birdBottom, obstacleLeft, obstacleMegHeight]);
+
+    useEffect(() => {
+      if (
+        (birdBottom < obstacleHeight + obstacleMegHeightTwo ||
+         birdBottom > obstacleHeight + obstacleMegHeightTwo + gap) &&
+        obstacleLeftTwo < birdLeft + 30 &&
+        obstacleLeftTwo + obstacleWidth > birdLeft
+      ) {
+        gameOver();
+      }
+    }, [birdBottom,
+       obstacleLeftTwo,
+       obstacleMegHeightTwo,
+       obstacleMegHeight,
+       obstacleLeft,
+       isGameOver]);
 
   useEffect(() => {
     if (birdBottom > 0) {
@@ -66,7 +114,9 @@ import { useEffect } from "react";
   }, [birdBottom]);
 
   return (
+    <TouchableWithoutFeedback onPress={Jump}>
     <View style={styles.container}>
+      <Text style={styles.score}>{score}</Text> 
       <Bird
         birdBottom={birdBottom}
         birdLeft={birdLeft}
@@ -86,15 +136,22 @@ import { useEffect } from "react";
         obstacleNegHeight={obstacleMegHeightTwo}
       />
     </View>
+    </TouchableWithoutFeedback>
   );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  score:{
+    position:"absolute",
+    top:50,
+    fontSize:24,
+    fontWeight:"bold",
+    zIndex:10
+  }
+});
